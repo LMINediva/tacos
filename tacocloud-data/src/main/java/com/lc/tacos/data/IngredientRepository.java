7790929e28d2bb8bd5b172fe46ef72635e3206d9
LMINediva <3037907347@qq.com>
2022-08-23 21:28:14 +0800

    启用数据后端服务

--- tacocloud-data/src/main/java/com/lc/tacos/data/IngredientRepository.java
+++ tacocloud-data/src/main/java/com/lc/tacos/data/IngredientRepository.java
@@ -2,11 +2,14 @@ package com.lc.tacos.data;
 
 import com.lc.tacos.domain.Ingredient;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  * @author DELL
  * @date 2022/4/19 17:14
  */
+@RepositoryRestResource(collectionResourceRel = "ingredients",
+        path = "ingredients")
 public interface IngredientRepository extends
         CrudRepository<Ingredient, String> {
 
--- tacocloud-data/src/main/java/com/lc/tacos/data/TacoRepository.java
+++ tacocloud-data/src/main/java/com/lc/tacos/data/TacoRepository.java
@@ -2,11 +2,14 @@ package com.lc.tacos.data;
 
 import com.lc.tacos.domain.Taco;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  * @author DELL
  * @date 2022/4/19 17:15
  */
+@RepositoryRestResource(collectionResourceRel = "tacos",
+        path = "tacos")
 public interface TacoRepository extends
         PagingAndSortingRepository<Taco, Long> {
 
